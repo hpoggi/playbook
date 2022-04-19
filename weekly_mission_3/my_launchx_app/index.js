f162e3f776d9831d075e5daab83c03fe5f6b135a
Hector Poggi <54382354+hpoggi@users.noreply.github.com>
2022-04-19 18:32:57 -0500

    Create index.js

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const fernanda = new MissionCommander("Fernanda")
+const rodrigo = new MissionCommander("Rodrigo")
+console.log(carlo.name)
+console.log(fernanda.name)
+console.log(rodrigo.name)
