abf1ac250caf0f2e6db99186cf2c8fd9bf4a8328
Hector Poggi <54382354+hpoggi@users.noreply.github.com>
2022-04-20 23:23:19 -0500

    Update UserView.test.js

--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -1,19 +1,20 @@
 const UserView = require('./../../app/views/UserView')
 
 describe("Tests for UserView", () => {
-    test("Return an error object when trying to create a new user with a null payload"), () => {
+
+    test("1) Return an error object when trying to create a new user with a null payload", () => {
         const payload = null
         const result = UserView.createUser(payload)
 
         expect(result.error).toMatch(/payload no existe/)
     })
-    
-    test("2) Return an error object when trying to create a new user with a payload with invalid properties"), () => {
+
+    test("2) Return an error object when trying to create a new user with a payload with invalid properties", () => {
         const payload = {username: null, name: 12, id: "id"}
         const result = UserView.createUser(payload)
 
         expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
 
-
 })
+
