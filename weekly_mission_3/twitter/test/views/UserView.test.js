08de9505248a48032f652fb97ad632ec231db3b1
Hector Poggi <54382354+hpoggi@users.noreply.github.com>
2022-04-20 23:14:30 -0500

    Create UserView.test.js

---
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -0,0 +1,10 @@
+const UserView = require('./../../app/views/UserView')
+
+describe("Tests for UserView", () => {
+    test("Return an error object when trying to create a new user with a null payload"), () => {
+        const payload = null
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/payload no existe/)
+    }
+})
