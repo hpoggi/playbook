ce3d63e288fe1c768ab31ef4bdafc53b823a15a2
Hector Poggi <54382354+hpoggi@users.noreply.github.com>
2022-04-20 23:15:39 -0500

    Update UserView.js

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -1,1 +1,9 @@
+const UserService = require('./../../services/UserService')
 
+class UserView {
+    static createUser(payload){
+        if(payload===null){
+            return {'error': 'payload no existe'}
+        }
+    }
+}
