7ca7bc0a6618d37d466299c10951ec68349964b7
Hector Poggi <54382354+hpoggi@users.noreply.github.com>
2022-04-20 22:43:42 -0500

    Update UserService.test.js

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -1,6 +1,7 @@
 const UserService = require('./../../app/services/UserService')
 
 describe("Tests for UserService", () => {
+    
     test("1. Create a new user using the UserService", () => {
         const user = UserService.create(1, "hpoggi", "Hector")
         expect(user.username).toBe("hpoggi")
@@ -8,5 +9,15 @@ describe("Tests for UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
+    
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1, "hpoggi", "Hector")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("hpoggi")
+        expect(userInfoInList[2]).toBe("Hector")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    })
+
 
 })
